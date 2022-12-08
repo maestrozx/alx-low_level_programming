f525baf23f30890f54433e3b472eeb11d4fff589
maestrozx <liberiusongere@gmail.com>
2022-12-08 10:36:18 -0800

    Printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,23 +1,11 @@
 #include <stdio.h>
-
-
-
 /**
-
- * main - prints a sentence with the printf function
-
- *
-
- * Return: returns 0 after printing
-
- */
-
+* main - prints a sentence with the printf function
+*
+* Return: returns 0 after printing
+*/
 int main(void)
-  
 {
-  
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  
-  return (0);
-  
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
