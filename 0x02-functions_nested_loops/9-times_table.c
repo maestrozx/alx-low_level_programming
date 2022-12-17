bcb9e8136f189a94ac07caeedebe2c650aa07e9d
maestrozx <118158683+maestrozx@users.noreply.github.com>
2022-12-17 13:12:03 +0300

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,13 +1,37 @@
 #include "main.h"
-#include <stdio.h>
 /**
-* add - Adds two integers.
-* num1: The first integer to be added.
-* num2: The second integer to be added.
+* times_table - Prints the 9 times table, starting with 0
 *
-* Return: The result of the addition.
+* Return: empty output
 */
-int add(int n1, int n2)
+void times_table(void)
 {
-return (n1 + n2);
+int x, y, z, u, d;
+for (x = 0; x <= 9; x++)
+{
+for (y = 0; y <= 9; y++)
+{
+z = x * y;
+if (z > 9)
+{
+u = z % 10;
+d = (z - u) / 10;
+_putchar(44);
+_putchar(32);
+_putchar(d + '0');
+_putchar(u + '0');
+}
+else
+{
+if (y != 0)
+{
+_putchar(44);
+_putchar(32);
+_putchar(32);
+}
+_putchar(z + '0');
+}
+}
+_putchar('\n');
+}
 }
