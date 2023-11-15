54ae7f02351b820456803459722a19b41d03fe7d
maestrozx <118158683+maestrozx@users.noreply.github.com>
2023-11-15 21:44:29 +0300

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 /**
+* _putchar - defines char to std output.
 * _puts_recursion - prints a string.
 * @str: string to be printed.
 * Return: Nothing.
 */
+int _putchar(char c)
+{
+return putchar(c);
+}
 void _puts_recursion(char *str)
 {
 if (*str == '\0')
