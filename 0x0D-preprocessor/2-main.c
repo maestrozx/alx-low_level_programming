4a0648e73c8a3d0c6285242f2e900c585eb96be2
maestrozx <liberiusongere@gmail.com>
2023-11-28 00:15:54 +0300

    Source file code

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
-#ifndef FILE
-#define FILE __FILE__
-#endif
-/*
-* main - Returns filename of sorce file.
+/**
+* main - Entry point of the program.
 *
-* Return: Always 0 indicating successful execution.
+* Description:
+* This program prints the name of the source file using the FILE macro.
+*
+* Return:
+* Always 0 to indicate successful execution.
 */
 int main(void)
 {
+/**
+* FILE - Macro representing the name of the current source file.
+*
+* Description:
+* This macro expands to a string literal containing the name of the
+* current source file.
+*/
+#define FILE __FILE__
 printf("The file name is: %s\n", FILE);
 return (0);
 }
