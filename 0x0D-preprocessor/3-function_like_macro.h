98f07852495c97ebcce2034b253d4969c0cd1b1f
maestrozx <liberiusongere@gmail.com>
2023-11-28 00:42:03 +0300

    Absolute Function

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,17 +1,4 @@
-#include <stdio.h>
-#include "3-function_like_macro.h"
-#include "3-function_like_macro.h"
-/**
-* main - tests the code
-*
-* Return: 0 if code is a success.
-*/
-int main(void)
-{
-int a;
-int b;
-a = ABS(-98) * 10;
-b = ABS(98) * 10;
-printf("%d, %d\n", a, b);
-return (0);
-}
+#ifndef ABSO
+#define ABSO
+#define ABS(X) (((X) < (0)) ? (-(X)) : (X))
+#endif /* ABS(X) */
