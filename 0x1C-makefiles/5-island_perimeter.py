ba43e1bce25a59cfa3c2abba8781938f14091210
maestrozx <liberiusongere@gmail.com>
2024-05-30 00:28:59 +0300

    Advanced Makefile

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,41 +1,26 @@
 #!/usr/bin/python3
 """
-The perimeter of the island
+Island Perimeter
 """
 
 
 def island_perimeter(grid):
-    """
-    Function that calculate the perimeter of the island
-    described in grid.
+    """ Returns the perimeter of the island described in grid.
 
     Args:
-       grid: Matrix that emulate the island grid.
-
-    Return:
-       Returns the perimeter of the island described in grid.
+        grid (int): list of list of integers.
 
     """
-    count = 0
-    connection_h = 0
-    connection_v = 0
-    # Count horizontal connection of numbers 1
-    for _list in grid:
-        i = 1
-        for number in _list:
-            if number == 1:
-                count += 1
-                if i < len(_list) and number == _list[i]:
-                    connection_h += 1
-            i += 1
-    # Count vertical connection of numbers 1
-    for index, _list in enumerate(grid):
-        for i in range(0, len(_list)):
-            if index < len(grid) - 1:
-                if _list[i] == 1 and _list[i] == grid[index + 1][i]:
-                    connection_v += 1
-    total = count * 4
-    horizontal = connection_h * 2
-    vertical = connection_v * 2
-    perimeter = total - horizontal - vertical
+    perimeter = 0
+
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j]:
+                perimeter += 4
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 2
+
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 2
+
     return perimeter
